29d8ec509206090fd088897fa4cedbc35d6aea84
Mevorah Berrebi <berrebimevo@cs.colman.ac.il>
2026-01-14 13:24:48 +0200

    connection between routes,server and controllers

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-}); 
+app.get("/posts", (req, res) => {
+  res.send("Getting all posts");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
